63c6bb227c956622c6326ef05240a1ad8dcd80d3
AdultEmby <adultemby@users.noreply.github.com>
2017-12-01 11:44:04 +0000

    Script to create src directory from an AngleSharp release

---
+++ README.md
@@ -0,0 +1,12 @@
+# AdultEmby.AngleSharp
+
+A project to repackage [AngleSharp](https://github.com/AngleSharp/AngleSharp) to allow [AdultEmby.Plugins](https://github.com/adultemby/AdultEmby.Plugins) to embed it in their assembly.
+
+### Running
+
+Open a Powershell window
+
+```
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process
+./create.ps1
+```
---
+++ create.ps1
@@ -0,0 +1,19 @@
+# Run this in the Powershell window to allow this script to run
+# Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process
+
+if(Test-Path -Path build ){
+    Remove-Item build -recurse
+}
+if(Test-Path -Path AngleSharp ){
+    Remove-Item src -recurse
+}
+
+New-Item build -type directory
+wget https://github.com/AngleSharp/AngleSharp/archive/v0.9.9.zip -OutFile build/AngleSharp.zip
+Expand-Archive build/AngleSharp.zip -DestinationPath build
+Copy-Item build/AngleSharp-0.9.9/src/AngleSharp src -recurse
+Remove-Item src/AngleSharp.Core.csproj
+Remove-Item src/AngleSharp.NETCore.xproj
+Remove-Item src/Key.snk
+Remove-Item src/project.json
+Remove-Item src/Properties/AssemblyInfo.cs
